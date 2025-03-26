de23e659eb91075a7c9ae442b31979760a9d9b92
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-26 19:37:47 -0300

    fix: Eliminar ParameterService porque porque no pertenece al módulo auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { ParameterService } from '@shared'
 
 @Module({
-  providers: [AuthService, ParameterService],
   exports: [AuthService]
 })
 export class AuthModule {}
