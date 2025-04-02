842402140d55ee76917c44c659800bc41b441a05
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-04-02 00:57:43 -0300

    fix: Actualizar configuración de módulos y eliminar AuthModule
    - Se eliminó el archivo AuthModule, simplificando la estructura del proyecto.
    - Se trasladó la lógica de creación de ApiKeyRepository al módulo TaskTrigger, integrando AuthService como proveedor.

--- src/task-trigger/task-trigger.module.ts
+++ src/task-trigger/task-trigger.module.ts
@@ -3,15 +3,27 @@ import { TaskTriggerService } from './task-trigger.service'
 import { createTaskRepository, TaskRepository } from '../task/task.repository'
 import { ParameterService } from '@shared'
 import { ScmModule } from '../scm/scm.module'
-import { AuthModule } from '../auth/auth.module'
-
+import { AuthService } from '../auth/auth.service'
+import { createApiKeyRepository, ApiKeyRepository } from '../api-key/api-key.repository'
 @Module({
   imports: [
-    ScmModule,
-    AuthModule
+    ScmModule
   ],
   providers: [
     TaskTriggerService,
+    AuthService,
+    {
+      provide: ApiKeyRepository,
+      useFactory: async (parameterService: ParameterService) => {
+        const apiKeyTable = await parameterService.get('dynamo-api-key-table-name')
+        return createApiKeyRepository({
+          tableName: apiKeyTable as string,
+          awsStage: process.env.AWS_STAGE ?? 'localstack',
+          awsEndpoint: process.env.AWS_ENDPOINT ?? 'http://localhost:4566'
+        })
+      },
+      inject: [ParameterService]
+    },
     {
       provide: TaskRepository,
       useFactory: async (parameterService: ParameterService) => {
