5e724d53f77c570103c858d1c506ab5082c83ab1
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-04-03 20:37:28 -0300

    fix: Eliminar módulo Logger y exportar TaskCliFilesRepository

--- src/task-cli-files/task-cli-files.module.ts
+++ src/task-cli-files/task-cli-files.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common'
-import { LoggerModule } from 'nestjs-pino'
-import { pino } from 'pino'
 import { createTaskCliFilesRepository, TaskCliFilesRepository } from './task-cli-files.repository'
 
 const awsStage = process.env.AWS_STAGE ?? 'localstack'
 const awsEndpoint = process.env.AWS_ENDPOINT ?? 'http://localhost:4566'
 
 @Module({
-  imports: [
-    LoggerModule.forRoot({
-      pinoHttp: {
-        level: process.env.LOG_LEVEL ?? 'info',
-        timestamp: pino.stdTimeFunctions.isoTime,
-        formatters: {
-          level (label: string): { level: string } {
-            return { level: label }
-          }
-        }
-      }
-    })
-  ],
   providers: [
     {
       useFactory: () => {
@@ -31,6 +16,7 @@ const awsEndpoint = process.env.AWS_ENDPOINT ?? 'http://localhost:4566'
       },
       provide: TaskCliFilesRepository
     }
-  ]
+  ],
+  exports: [TaskCliFilesRepository]
 })
 export class TaskCliFilesModule {}
