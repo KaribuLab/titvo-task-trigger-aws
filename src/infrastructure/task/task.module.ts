bd1453446adf4dcdf1997460d38d515e141681a8
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-13 00:10:03 -0400

    fix: Eliminar importación de ValidateApiKeyUseCase y CryptoModule del TaskModule

--- src/infrastructure/task/task.module.ts
+++ src/infrastructure/task/task.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common'
 import { createTaskRepository } from '@infrastructure/task/task.dynamo'
 import { TaskRepository, TriggerTaskUseCase } from '@titvo/trigger'
-import { ValidateApiKeyUseCase } from '@titvo/auth'
 import { ApiKeyModule } from '@infrastructure/api-key/api-key.module'
-import { CryptoModule } from '@infrastructure/crypto/crypto.module'
 import { CliFilesModule } from '@infrastructure/cli-files/cli-files.module'
 import { ScmModule } from '@infrastructure/scm/scm.module'
 @Module({
   providers: [
-    ValidateApiKeyUseCase,
     TriggerTaskUseCase,
     {
       provide: TaskRepository,
@@ -18,7 +15,7 @@ import { ScmModule } from '@infrastructure/scm/scm.module'
         awsEndpoint: process.env.AWS_ENDPOINT as string
       })
     }],
-  imports: [ApiKeyModule, CryptoModule, CliFilesModule, ScmModule],
+  imports: [ApiKeyModule, CliFilesModule, ScmModule],
   exports: [TriggerTaskUseCase]
 })
 export class TaskModule {}
